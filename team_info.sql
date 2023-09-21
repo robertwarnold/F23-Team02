89125178dd6391797aa5eeda59e011222f5d782a
robertwarnold <robertwarnold13@gmail.com>
2023-09-21 16:42:48 -0400

    This is the second commit, it contains the new sql code for the db

---
+++ team_info.sql
@@ -0,0 +1,8 @@
+-- Create a table to store team information
+CREATE TABLE team_info (
+    team_number 2 PRIMARY KEY,
+    project_name 'Capstone' NOT NULL,
+    project_description "This is the capstone project for Team 2 - BATHH.",
+    version_number 1.0,
+    release_date 09/21/23
+);
