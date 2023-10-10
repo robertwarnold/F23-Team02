b10214454e6c1976cb4f759323dc27d8f013becf
Sam Austin <samuelaa02@gmail.com>
2023-10-10 12:37:51 -0400

    added config.js to contain app information

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_db0YqmgDq',
+        userPoolClientId: '6e4rn9da6dl0uir1s0o9judpnd',
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
