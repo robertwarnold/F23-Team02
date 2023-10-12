d5c781cf5a060c82a74671eb940f089d31ad1797
Sam Austin <samuelaa02@gmail.com>
2023-10-11 20:19:50 -0400

    added testing files to receive data from api gateway and lambda

---
+++ testing/testing.js
@@ -0,0 +1,11 @@
+function lambdaTest(){
+    fetch('https://u76zsrtgq8.execute-api.us-east-1.amazonaws.com/team02-testing')
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+        document.getElementById("test").innerHTML = data.message;
+    })
+    .catch(error => console.log(error));
+}
