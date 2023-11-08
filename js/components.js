7326f0f3ad670c35e3802582d4e4da644cc48551
mbaldiz23 <mbaldiz@clemson.edu>
2023-11-08 11:31:28 -0500

    create foundation of add to cart functionality

--- js/components.js
+++ js/components.js
@@ -101,14 +101,19 @@ customElements.define("catalog-item", catalogItem);
             document.querySelector('#not_match').style.display = 'none';
 
             for (var i = 0; i < obj.results.length; i++) {
+                var albumArt = obj.results[i].artworkUrl100;
+                var album = obj.results[i].collectionName;
+                var artist = obj.results[i].artistName;
+                var price = Math.round(obj.results[i].collectionPrice * 100)
+
                 template += '<div class="col-sm-3 album_item" id="album_item">';
-                template += '<div class="item_thmb" id="album_art" style="background:url(' + obj.results[i].artworkUrl100 + ')"></div>';
+                template += '<div class="item_thmb" id="album_art" style="background:url(' + albumArt + ')"></div>';
                 template += '<div id="song_info">';
-                template += '<div class="item_title"> <p style="font-weight: bold; display: inline-block;">Album: </p>' + obj.results[i].collectionName + '</div>';
-                template += '<div class="artist_name"> <p style="font-weight: bold; display: inline-block;">Artist: </p>' + obj.results[i].artistName + '</p></div>';
-                template += '<div class="item_price"> <p style="font-weight: bold; display: inline-block;">Price: </p>' + obj.results[i].collectionPrice * 100 + ' points </div>';
+                template += '<div class="item_title"> <p style="font-weight: bold; display: inline-block;">Album: </p>' + album + '</div>';
+                template += '<div class="artist_name"> <p style="font-weight: bold; display: inline-block;">Artist: </p>' + artist + '</p></div>';
+                template += '<div class="item_price"> <p style="font-weight: bold; display: inline-block;">Price: </p>' + price + ' points </div>';
                 template += '</div>';
-                template += '<a id="buy" href="' + obj.results[i].collectionViewUrl + '" target="_blank">Add To Cart</a>';
+                template += '<button id="buy" onclick="addToCart(this, \'' + obj.results[i].artworkUrl100 + '\', \'' + obj.results[i].collectionName + '\', \'' + obj.results[i].artistName + '\', \'' + Math.round(obj.results[i].collectionPrice * 100) + '\')" target="_blank">Add To Cart</button>';
                 template += '</div>';
             }
 
@@ -130,3 +135,62 @@ customElements.define("catalog-item", catalogItem);
     this.init();
 
 })();
+
+// start of modal js
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+/* Add To Cart */
+
+function addToCart(btn, albumArt, album, artist, price) {
+    btn.innerHTML = "Added!"
+
+    var template = "";
+    
+    template += '<div class="col-sm-3 album_item" id="mini_album_item">';
+    template += '<div class="item_thmb" id="album_art" style="background:url(' + albumArt + ')"></div>';
+    template += '<div id="song_info">';
+    template += '<div class="item_title"> <p style="font-weight: bold; display: inline-block;">Album: </p>' + album + '</div>';
+    template += '<div class="artist_name"> <p style="font-weight: bold; display: inline-block;">Artist: </p>' + artist + '</p></div>';
+    template += '<div class="item_price"> <p style="font-weight: bold; display: inline-block;">Price: </p>' + price + ' points </div>';
+    template += '</div>';
+    template += '</div>';
+    template += "<hr>";
+    
+    document.getElementById("cart").innerHTML += template;
+}
